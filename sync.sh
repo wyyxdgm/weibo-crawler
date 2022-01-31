f2fee525d53dc40f4c54009dcad8895ab4a46292
Damo <wyyxdgm@163.com>
2022-01-31 16:13:50 +0800

    feat:prod sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -avz --progress . root@101.43.109.138:weibo-crawler --exclude weibo --exclude .git
+rsync -avz --progress . root@101.43.109.138:weibo-crawler --exclude weibo --exclude .git --exclude config.json
+rsync -avz --progress config.prod.json root@101.43.109.138:weibo-crawler/config.json
