7d10077e53cd5318e1449573ce5c4645b4f63932
Damo <wyyxdgm@163.com>
2022-02-03 00:18:28 +0800

    feat:exclue user_id_list.txt

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -avz --progress . root@101.43.109.138:weibo-crawler --exclude weibo --exclude .git --exclude config.json --exclude all.log --exclude error.log
+rsync -avz --progress . root@101.43.109.138:weibo-crawler --exclude weibo --exclude .git --exclude config.json --exclude all.log --exclude error.log --exclude user_id_list.txt
 rsync -avz --progress config.prod.json root@101.43.109.138:weibo-crawler/config.json
