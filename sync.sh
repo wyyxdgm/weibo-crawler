65978230d2fe77ba54f5196eb7a37e9e5f7868ee
Damo <wyyxdgm@163.com>
2022-02-17 00:19:49 +0800

    feat:sync ignore log

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -avz --progress . root@101.43.109.138:jobs-crawler --exclude weibo --exclude .git --exclude config.json --exclude all.log --exclude error.log --exclude user_id_list.txt
+rsync -avz --progress . root@101.43.109.138:jobs-crawler --exclude weibo --exclude .git --exclude config.json --exclude all.log --exclude error.log --exclude user_id_list.txt --exclude all-get-jobs.log --exclude error-get-jobs.log
 rsync -avz --progress config.prod.json root@101.43.109.138:jobs-crawler/config.json
